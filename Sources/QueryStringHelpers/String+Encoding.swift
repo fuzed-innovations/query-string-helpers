42dd2d6956fce0824b099d2931730033545fca46
Ryan Peggs <ryanpeggs@Ryans-MacBook-Pro-2.local>
2017-11-19 23:07:06 +0000

    Add string encoding extension

---
+++ Sources/QueryStringHelpers/String+Encoding.swift
@@ -0,0 +1,40 @@
+//
+//  String+Encoding.swift
+//  QueryStringHelpers
+//
+//  Created by Ryan Peggs on 19/11/2017.
+//
+
+import Foundation
+
+public enum URLQuerySpaceEncodingMode {
+    case plus
+    case percent
+}
+
+extension String {
+    
+    public func encodedForURLQuery(spacesMode: URLQuerySpaceEncodingMode) -> String {
+        
+        let urlQueryAllowed = [
+            CharacterSet(charactersIn: "a"..."z"),
+            CharacterSet(charactersIn: "A"..."Z"),
+            CharacterSet(charactersIn: ".-~_")
+            ].reduce(CharacterSet()) { combined, next -> CharacterSet in
+                return combined.union(next)
+        }
+        
+        switch spacesMode {
+        case .plus:
+            let allowedCharacters = urlQueryAllowed.union(CharacterSet(charactersIn: " "))
+            let encoded = self.addingPercentEncoding(withAllowedCharacters: allowedCharacters) ?? ""
+            return encoded.replacingOccurrences(of: " ", with: "+")
+        case .percent:
+            return self.addingPercentEncoding(withAllowedCharacters: urlQueryAllowed) ?? ""
+        }
+    }
+    
+    public func decodedFromURLQuery() -> String {
+        return self.replacingOccurrences(of: "+", with: " ").removingPercentEncoding ?? ""
+    }
+}
